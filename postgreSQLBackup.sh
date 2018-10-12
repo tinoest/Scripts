979af1a169041e1f0c9962199ccdaaf91c59e8d9
Martyn Brown <tinoest@gmail.com>
2018-10-12 22:24:30 +0100

    Removed whitespace

--- postgreSQLBackup.sh
+++ postgreSQLBackup.sh
@@ -8,10 +8,10 @@ echo "================================="
 echo `date`;
 for database in `/sw/bin/psql -U postgres -lt | awk '{print $1}' | grep -vE '\||^$|template0'`;
 do
-				printf "Exporting $database...";
-				/sw/bin/pg_dump -U postgres -c $database | gzip -c > $backupdir/$day/$database.sql.gz;
-				/bin/chmod 600 $backupdir/$day/$database.sql.gz;    
-				printf "done\n";
+	printf "Exporting $database...";
+	/sw/bin/pg_dump -U postgres -c $database | gzip -c > $backupdir/$day/$database.sql.gz;
+	/bin/chmod 600 $backupdir/$day/$database.sql.gz;    
+	printf "done\n";
 done
 echo `date`;
 echo;
